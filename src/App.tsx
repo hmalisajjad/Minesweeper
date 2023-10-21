2eb7a3038529342d1d3101ef476b1da9507e4b87
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2023-10-22 00:30:24 +0200

    JSX initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header logo={logo}>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Link href="https://reactjs.org" target="_blank">
+          Learn react
+        </Link>
+      </Header>
     </div>
   );
 }
+
+interface HeaderProps {
+  children: React.ReactElement;
+  logo: string;
+}
+
+const Header= ({
+  children,
+  logo,
+}:HeaderProps): React.ReactNode => {
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    {children}
+  </header>;
+};
+
+interface LinkProps {
+  href: string;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+  rel?: string;
+  children: React.ReactNode;
+}
+
+const Link: FC<LinkProps> = ({
+  children,
+  href,
+  target = "_blank",
+  rel = "noopener noreferrer",
+}) => (
+  <a className="App-Link" href={href} target={target} rel="noopener noreferrer">
+    {children}
+  </a>
+);
 
 export default App;
