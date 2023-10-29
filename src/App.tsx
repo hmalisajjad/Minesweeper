859a536a12a6bb4c055d22127461af97ee3dcb40
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2023-10-29 12:17:22 +0100

    webpack dev server

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Header>
+      <Header logo={logo}>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
